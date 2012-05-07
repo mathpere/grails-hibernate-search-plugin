f7888a0d27f1be87b03d56c2655d44940b0a4198
Mathieu Perez <mathieu.perez@novacodex.net>
2012-05-07 20:47:36 +0200

    remove useless usage of persistent method

--- HibernateSearchGrailsPlugin.groovy
+++ HibernateSearchGrailsPlugin.groovy
@@ -3,6 +3,10 @@ import org.codehaus.groovy.grails.plugins.hibernate.search.SearchMappingConfigur
 import org.codehaus.groovy.grails.plugins.hibernate.search.SearchPersistentMethod
 import org.hibernate.search.annotations.Indexed
 import org.springframework.core.annotation.AnnotationUtils
+import org.springframework.orm.hibernate3.HibernateTemplate
+import org.hibernate.Session
+import org.codehaus.groovy.grails.plugins.hibernate.search.HibernateSearchQueryBuilder
+import org.springframework.orm.hibernate3.HibernateCallback
 
 class HibernateSearchGrailsPlugin {
     def version = "0.6.0"
@@ -15,14 +19,13 @@ class HibernateSearchGrailsPlugin {
     def documentation = "http://grails.org/plugin/hibernate-search"
     def license = 'APACHE'
     def organization = [name: 'NovaCodex', url: 'http://www.novacodex.net/']
-    def developers = [
-            [name: 'Mathieu Perez', email: 'mathieu.perez@novacodex.net'],
+    def developers = [[name: 'Mathieu Perez', email: 'mathieu.perez@novacodex.net'],
             [name: 'Julie Ingignoli', email: 'julie.ingignoli@novacodex.net']]
     def issueManagement = [system: 'github', url: 'https://github.com/mathpere/grails-hibernate-search-plugin/issues']
     def scm = [url: 'https://github.com/mathpere/grails-hibernate-search-plugin']
 
     def doWithSpring = {
-        sessionFactory(SearchMappingConfigurableLocalSessionFactoryBean) { bean ->
+        sessionFactory( SearchMappingConfigurableLocalSessionFactoryBean ) { bean ->
             // see org.codehaus.groovy.grails.plugins.orm.hibernate.HibernatePluginSupport:
             bean.parent = 'abstractSessionFactoryBeanConfig'
         }
@@ -30,17 +33,19 @@ class HibernateSearchGrailsPlugin {
 
     def doWithDynamicMethods = { ctx ->
 
-        def sessionFactory = ctx.getBean("sessionFactory")
+        def sessionFactory = ctx.sessionFactory
+
+        def hibernateTemplate = new HibernateTemplate( sessionFactory )
 
         // add search() method to indexed domain classes:
         application.domainClasses.each { grailsClass ->
             def clazz = grailsClass.clazz
 
-            if ( ClassPropertyFetcher.forClass(clazz).getStaticPropertyValue("search", Closure) || AnnotationUtils.isAnnotationDeclaredLocally(Indexed, clazz) ) {
-                def searchMethod = new SearchPersistentMethod(sessionFactory, application.classLoader, application)
-
-                grailsClass.metaClass.static.search = { Closure closure ->
-                    searchMethod.invoke(clazz, "search", [closure] as Object[])
+            if ( ClassPropertyFetcher.forClass( clazz ).getStaticPropertyValue( "search", Closure ) || AnnotationUtils.isAnnotationDeclaredLocally( Indexed, clazz ) ) {
+                grailsClass.metaClass.static.search = {
+                    hibernateTemplate.execute( { Session session ->
+                        new HibernateSearchQueryBuilder( clazz, session )
+                    } as HibernateCallback )
                 }
             }
         }
