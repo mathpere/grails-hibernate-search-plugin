fff039387c15d4326febab22a0b5bfbd4a8aeb82
Mathieu Perez <mathieu.perez@novacodex.net>
2012-08-21 10:08:05 +0200

    prepare release 0.7

--- HibernateSearchGrailsPlugin.groovy
+++ HibernateSearchGrailsPlugin.groovy
@@ -9,7 +9,7 @@ import org.springframework.orm.hibernate3.HibernateCallback
 import org.springframework.orm.hibernate3.HibernateTemplate
 
 class HibernateSearchGrailsPlugin {
-    def version = "0.6.1"
+    def version = "0.7"
     def grailsVersion = "2.0 > *"
     def loadAfter = ['hibernate']
     def title = "Hibernate Search Plugin"
