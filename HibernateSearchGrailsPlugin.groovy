6b39ec8d0deca7ee69e5a267b9007952aa336037
Jappie Klooster <jappieklooster@hotmail.com>
2013-12-31 12:40:26 +0100

    retabbed and a first try at configuring the plugin to work with the new hibernate api

--- HibernateSearchGrailsPlugin.groovy
+++ HibernateSearchGrailsPlugin.groovy
@@ -1,78 +1,70 @@
+package org.codehaus.groovy.grails.plugins.hibernate.search 
+
 import org.codehaus.groovy.grails.commons.ClassPropertyFetcher
 import org.codehaus.groovy.grails.plugins.hibernate.search.HibernateSearchConfig
 import org.codehaus.groovy.grails.plugins.hibernate.search.HibernateSearchQueryBuilder
 import org.codehaus.groovy.grails.plugins.hibernate.search.SearchMappingConfigurableLocalSessionFactoryBean
 import org.hibernate.Session
 import org.hibernate.search.annotations.Indexed
+import org.hibernate.search.Search
 import org.springframework.core.annotation.AnnotationUtils
-import org.springframework.orm.hibernate3.HibernateCallback
-import org.springframework.orm.hibernate3.HibernateTemplate
 
 class HibernateSearchGrailsPlugin {
-    def version = "0.81"
-    def grailsVersion = "2.3.3"
-    def loadAfter = ['hibernate']
-    def title = "Hibernate Search Plugin"
-    def author = "Mathieu Perez, Julie Ingignoli"
-    def authorEmail = "mathieu.perez@novacodex.net, julie.ingignoli@novacodex.net"
-    def description = 'Integrates Hibernate Search features to Grails'
-    def documentation = "http://grails.org/plugin/hibernate-search"
-    def license = 'APACHE'
-    def organization = [name: 'NovaCodex', url: 'http://www.novacodex.net/']
-    def developers = [[name: 'Mathieu Perez', email: 'mathieu.perez@novacodex.net'],
-            [name: 'Julie Ingignoli', email: 'julie.ingignoli@novacodex.net'],
+	def version = "0.81"
+	def grailsVersion = "2.3.3"
+	def loadAfter = ['hibernate']
+	def title = "Hibernate Search Plugin"
+	def author = "Mathieu Perez, Julie Ingignoli"
+	def authorEmail = "mathieu.perez@novacodex.net, julie.ingignoli@novacodex.net"
+	def description = 'Integrates Hibernate Search features to Grails'
+	def documentation = "http://grails.org/plugin/hibernate-search"
+	def license = 'APACHE'
+	def organization = [name: 'NovaCodex', url: 'http://www.novacodex.net/']
+	def developers = [[name: 'Mathieu Perez', email: 'mathieu.perez@novacodex.net'],
+			[name: 'Julie Ingignoli', email: 'julie.ingignoli@novacodex.net'],
 				[name: 'Jappie Klooster', email: 'jappieklooster@hotmail.com']]
-    def issueManagement = [system: 'github', url: 'https://github.com/mathpere/grails-hibernate-search-plugin/issues']
-    def scm = [url: 'https://github.com/mathpere/grails-hibernate-search-plugin']
+	def issueManagement = [system: 'github', url: 'https://github.com/mathpere/grails-hibernate-search-plugin/issues']
+	def scm = [url: 'https://github.com/mathpere/grails-hibernate-search-plugin']
 
-    def doWithSpring = {
-        sessionFactory( SearchMappingConfigurableLocalSessionFactoryBean ) { bean ->
-            // see org.codehaus.groovy.grails.plugins.orm.hibernate.HibernatePluginSupport:
-            bean.parent = 'abstractSessionFactoryBeanConfig'
-        }
-    }
+	def doWithSpring = {
+		sessionFactory( SearchMappingConfigurableLocalSessionFactoryBean ) { bean ->
+			// see org.codehaus.groovy.grails.plugins.orm.hibernate.HibernatePluginSupport:
+			bean.parent = 'abstractSessionFactoryBeanConfig'
+		}
+	}
 
-    def doWithDynamicMethods = { ctx ->
+	def doWithDynamicMethods = { ctx ->
 
-        def sessionFactory = ctx.sessionFactory
+		Session hiberTextSes = Search.getFullTextSession(
+				ctx.sessionFactory.currentSession
+		) 
 
-        def hibernateTemplate = new HibernateTemplate( sessionFactory )
+		// add search() method to indexed domain classes:
+		application.domainClasses.each { grailsClass ->
+			def clazz = grailsClass.clazz
 
-        // add search() method to indexed domain classes:
-        application.domainClasses.each { grailsClass ->
-            def clazz = grailsClass.clazz
+			if ( ClassPropertyFetcher.forClass( clazz ).getStaticPropertyValue( "search", Closure ) || AnnotationUtils.isAnnotationDeclaredLocally( Indexed, clazz ) ) {
+					
+				grailsClass.metaClass.static.search = {
+					new HibernateSearchQueryBuilder(clazz, hiberTextSes)
+				}
 
-            if ( ClassPropertyFetcher.forClass( clazz ).getStaticPropertyValue( "search", Closure ) || AnnotationUtils.isAnnotationDeclaredLocally( Indexed, clazz ) ) {
+				grailsClass.metaClass.search = {
+					new HibernateSearchQueryBuilder(clazz, delegate, hiberTextSes)
+				}
+			}
+		}
 
-                grailsClass.metaClass.static.search = {
-                    hibernateTemplate.execute( { Session session ->
-                        new HibernateSearchQueryBuilder( clazz, session )
-                    } as HibernateCallback )
-                }
+		// config
+		def hibernateSearchConfig = application.config.grails.plugins.hibernatesearch
 
-                grailsClass.metaClass.search = {
-                    def instance = delegate
-                    hibernateTemplate.execute( { Session session ->
-                        new HibernateSearchQueryBuilder( clazz, instance, session )
-                    } as HibernateCallback )
-                }
-            }
-        }
+		if ( hibernateSearchConfig && hibernateSearchConfig instanceof Closure ) {
 
-        // config
-        def hibernateSearchConfig = application.config.grails.plugins.hibernatesearch
+			hibernateSearchConfig.delegate = new HibernateSearchConfig( hiberTextSes )
+			hibernateSearchConfig.resolveStrategy = Closure.DELEGATE_FIRST
+			hibernateSearchConfig.call()
 
-        if ( hibernateSearchConfig && hibernateSearchConfig instanceof Closure ) {
+		}
 
-            hibernateTemplate.execute( { Session session ->
-
-                hibernateSearchConfig.delegate = new HibernateSearchConfig( session )
-                hibernateSearchConfig.resolveStrategy = Closure.DELEGATE_FIRST
-                hibernateSearchConfig.call()
-
-            } as HibernateCallback )
-
-        }
-
-    }
+	}
 }
