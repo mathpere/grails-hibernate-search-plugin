bcfbe7c0abd4bc5b2ad139232da412335e899f8f
Jappie Klooster <jappieklooster@hotmail.com>
2014-01-07 09:40:13 +0100

    The bridge now builds, with the weird generic constructs

--- src/groovy/org/codehaus/groovy/grails/plugins/hibernate/search/bridge/IdCollectionBridge.groovy
+++ src/groovy/org/codehaus/groovy/grails/plugins/hibernate/search/bridge/IdCollectionBridge.groovy
@@ -1,14 +1,37 @@
 package org.codehaus.groovy.grails.plugins.hibernate.search.bridge
+import java.lang.reflect.ParameterizedType
 
-class IdCollectionBridge extends IdBridge{
-
+class IdCollectionBridge<ObjectArray extends Collection> extends IdBridge{
 	@Override
 	String objectToString(Object object){
-	
+		Collection col = (Collection) object
+		String result = ""
+		col.each{
+			result += super.objectToString(it)
+		}
 	}
 
-	@Override stringToObject(String stringValue){
-	
+	@Override 
+	Object stringToObject(String stringValue){
+
+		Collection col = createCollection()
+
+		stringValue.split(",").each{
+			col.add(super.stringToObject(it))	
+		}
+		return col
+	}
+
+	/** a java can't create new generic workaround */
+	private static Collection createCollection(){
+		return (
+			(Class)(
+				(ParameterizedType) this.getClass().getGenericSuperclass()
+
+			).getActualTypeArguments()[0]
+
+		).newInstance(); 
 	}
 
 }
+
